32d4c476c3ed7bf8f6503ba30116ec3417978ddc
KonradMarzec1991 <konrimarzec@gmail.com>
2020-12-12 23:06:41 +0100

    Add `get_client_ip` function in utils

---
+++ server/message/utils.py
@@ -0,0 +1,13 @@
+"""
+Helper functions module
+"""
+
+
+def get_client_ip(request):
+    """Gets IP of given request"""
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
